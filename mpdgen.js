dccd6e26fcc0a34c5faf13af8386480f7c0193bd
film6090 <film6090@gmail.com>
2022-06-06 17:31:29 +0700

    fix a minor bug

--- mpdgen.js
+++ mpdgen.js
@@ -3,7 +3,7 @@ const { exec } = require('node:child_process');
 class mpdgen{
     constructor(filename){
 		var path = __dirname + '\\video\\'
-        //exec('ffmpeg.exe -f webm_dash_manifest -i '+path+filename+'_160x90_output.webm -f webm_dash_manifest -i '+path+filename+'_320x180_output.webm -f webm_dash_manifest -i '+path+filename+'_640x360_output.webm -f webm_dash_manifest -i '+path+filename+'_audio.webm -c copy -map 0 -map 1 -map 2 -map 3 -f webm_dash_manifest -adaptation_sets "id=0,streams=0,1,2 id=1,streams=3" '+path+filename+'_manifest.webm', (error, stdout, stderr) => {
+        exec('ffmpeg.exe -f webm_dash_manifest -i '+path+filename+'_160x90_output.webm -f webm_dash_manifest -i '+path+filename+'_320x180_output.webm -f webm_dash_manifest -i '+path+filename+'_640x360_output.webm -f webm_dash_manifest -i '+path+filename+'_audio.webm -c copy -map 0 -map 1 -map 2 -map 3 -f webm_dash_manifest -adaptation_sets "id=0,streams=0,1,2 id=1,streams=3" '+path+filename+'_manifest.webm', (error, stdout, stderr) => {
             if (error) {
               console.error(`exec error: ${error}`);
               return;
