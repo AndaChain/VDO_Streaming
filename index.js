f63669443d9517f4aec6836a16fdfe60559cfe88
film6090 <film6090@gmail.com>
2022-06-08 15:30:05 +0700

    Streaming Reworks(Still not working)

--- index.js
+++ index.js
@@ -1,60 +1,93 @@
-const express = require("express"); // เฟม work ของ nodejs
-const app = express();
+require("dotenv").config();
 
-const Streaming_read_index = require("./Streaming_read_index.js");
-const Streaming_index = require("./Streaming_index.js");
-const Streaming = require("./Streaming.js");
 const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const iplocation = require("iplocation");
+var video_list = null;
 
-const Create_Segment = require("./Segment_Creater.js"); 
-const mpdgen = require("./mpdgen.js");
-const jsongen = require("./jsongen.js");
+app.set("view engine", "ejs");
 
-//Use http://localhost:8000/ to acess the file.
-
-app.get("/",function(req,res){
-  res.redirect("/index/Polkka_rock.mp4") // แบบนี้ไปหน้าตามลิงค์
-  // res.sendFile(__dirname + "/index.html");
-})
-
-app.get("/index/:filename", function (req, res ) {
-  filename = req.params.filename;
-  res.sendFile(__dirname + "/index.html");
+app.use((req, res, next) => {
+	console.log("Request: ", req.url);
+	console.log("Received request w/ headers:", req.headers);
+	next();
 });
 
-app.get("/video", function (req, res) {
-  const range = req.headers.range
-  var dir = __dirname + "/video/" + filename;
-  new Streaming(req, res, range, dir);
-  // new Streaming_index(dir);
-  // new Streaming_read_index(req, res, range, dir);
+if (process.env["NODE_ENV"] === "production") {
+	app.use((req, res, next) => {
+		let ip_addr = (req.headers["x-forwarded-for"] || req.connection.remoteAddress).split(",")[0];
+
+		iplocation(ip_addr, (err, info) => {
+			console.log(info);
+			if (info && (info["country_code"] === process.env["COUNTRY_CODE"] || info["time_zone"].includes(process.env["KEY_WORD"]))) {
+				res.redirect(process.env["OTHER_URL"]);
+			} else {
+				next();
+			}
+		});
+	});
+}
+;
+
+const resolve_file_path = (video_id, filename) => path.join(__dirname, "video", video_id, filename);
+const resolve_content_type = (filename) => filename === "audio.webm" ? "audio/webm" : "video/webm";
+
+app.get("/", (req, res) => {
+	if (!video_list) {
+		let video_mnt_path = path.join(__dirname, "video");
+		let folders = fs.readdirSync(video_mnt_path).filter((name) => {
+			return fs.lstatSync(path.join(video_mnt_path, name)).isDirectory();
+		});
+		video_list = folders;
+	}
+
+	res.locals.video_list = video_list;
+	res.render("index")
 });
 
-app.get("/CreateIndex",function(req,res){
-  new Streaming_index(__dirname+"/index/Polkka_rock.mp4");
-  res.redirect("/");
+app.get("/watch/:video_id", (req, res) => {
+	fs.stat(path.join(__dirname, "video", req.params["video_id"]), (err, stats) => {
+		if (err || !stats.isDirectory()) {
+			console.log(err);
+			return res.redirect("/");
+		}
+
+		res.locals.video_id = req.params["video_id"];
+		res.render("player");
+	});
 });
 
-app.get("/CreateSegment",function(req,res){
-  var segment = new Create_Segment(__dirname+"\\video\\" , "Polkka_rock");
-  segment.extract_video('640x360')
-  segment.extract_video('320x180')
-  segment.extract_video('160x90')
-  segment.extract_audio()
-  res.redirect("/");
-})
-
-app.get("/CreateMPD" , function(req,res){
-  new mpdgen("Polkka_rock");
-  res.redirect("/");
-})
-
-app.get("/CreateJSON" , function(req,res){
-  new jsongen("Polkka_rock");
-  res.redirect("/");
-})
-
-app.listen(8000, function () {
-  console.log("Listening on port 8000!");
+app.get("/watch/:video_id/manifest.mpd", (req, res) => {
+	res.sendFile(resolve_file_path(req.params["video_id"], "manifest.mpd"));
 });
 
+app.get("/watch/:video_id/timestamps/:filename", (req, res) => {
+	res.sendFile(resolve_file_path(req.params["video_id"], `timestamps/${req.params["filename"]}`));
+});
+
+app.get("/watch/:video_id/:filename", (req, res) => {
+	let file_path = resolve_file_path(req.params["video_id"], req.params["filename"]);
+
+	let { size: fileSize } = fs.statSync(file_path);
+
+	const parts = req.headers.range.replace(/bytes=/, "").split("-");
+	const start = parseInt(parts[0], 10);
+	const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1;
+
+	let chunkSize = end - start + 1;
+	let head = {
+		"Transfer-Encoding": "chunked",
+		"Content-Range": `bytes ${start}-${end}/${fileSize}`,
+		"Accept-Ranges": "bytes",
+		"Content-Length": chunkSize,
+		"Content-Type": resolve_content_type(req.params.filename)
+	};
+	res.writeHead(206, head);
+
+	fs.createReadStream(file_path, { start, end }).pipe(res);
+});
+
+app.listen(PORT, () => console.log(`Server listening in on port ${PORT}`));
