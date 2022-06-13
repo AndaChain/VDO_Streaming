3ed7fc6b1e12f081d2b902f1b055787140ea9aa5
film6090 <film6090@gmail.com>
2022-06-13 16:16:38 +0700

    Update streaming module
    #Need to update manifest gen to exclude path and this version still now working

--- Streaming/Queue.js
+++ Streaming/Queue.js
@@ -2,10 +2,24 @@ class Queue {
 	constructor() {
 		this.data = [];
 		this.pipingToSourceBuffer = false;
+		this.numBytesWrittenInSegment = 0;
 	}
 
-	push(el) {
-		this.data.push(el)
+	push(buf) {
+		if (!buf) {
+			throw new Error("Cannot push falsey values to queue");
+		}
+
+		this.data.push(buf);
+		this.numBytesWrittenInSegment += buf.length
+	}
+
+	resetByteCounter() {
+		this.numBytesWrittenInSegment = 0;
+	}
+
+	empty() {
+		return this.data.length === 0;
 	}
 
 	popFirst() {
